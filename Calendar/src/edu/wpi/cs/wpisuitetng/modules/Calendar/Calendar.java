22e916d5059e4ae6b8f404d14e1deb278408700f
Zach <Zach@zpjasensky-macbookpro.wifi.wpi.edu>
2013-11-06 18:29:54 -0500

    test

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/Calendar/Calendar.java
@@ -0,0 +1,54 @@
+package edu.wpi.cs.wpisuitetng.modules.Calendar;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
+import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+
+public class Calendar implements IJanewayModule {
+	
+	List<JanewayTabModel> tabs;
+	
+	public Calendar() {
+		tabs = new ArrayList<JanewayTabModel>();
+		
+		// Create a JPanel to hold the toolbar for the tab
+		JPanel toolbarPanel = new JPanel();
+		toolbarPanel.add(new JLabel("Calendar toolbar placeholder")); // add a label with some placeholder text
+		toolbarPanel.setBorder(BorderFactory.createLineBorder(Color.blue, 2)); // add a border so you can see the panel
+
+		// Create a JPanel to hold the main contents of the tab
+		JPanel mainPanel = new JPanel();
+		mainPanel.add(new JLabel("Calendar placeholder"));
+		mainPanel.setBorder(BorderFactory.createLineBorder(Color.green, 2));
+
+		// Create a tab model that contains the toolbar panel and the main content panel
+		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolbarPanel, mainPanel);
+
+		// Add the tab to the list of tabs owned by this module
+		tabs.add(tab1);
+		
+		
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Calendar";
+	}
+
+	@Override
+	public List<JanewayTabModel> getTabs() {
+		// TODO Auto-generated method stub
+		return tabs;
+	}
+
+}
