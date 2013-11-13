5e86381791a826bd63120e047abaff987391b43c
sdiamond492 <sdiamond492@sdiamond492-VirtualBox>
2013-11-13 18:41:15 -0500

    Added source folder test that includes JUnit test classes

---
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/model/EventManagerTest.java
@@ -0,0 +1,69 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventManagerTest {
+
+	@Test
+	public void testEventManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvancedGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvancedPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvancedPost() {
+		fail("Not yet implemented");
+	}
+
+}
