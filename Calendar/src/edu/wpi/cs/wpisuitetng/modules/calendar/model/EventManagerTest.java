7ad752597d1db08450418c2e1d1a4fcd4ed88ff7
John French <jdaviesf7@gmail.com>
2013-11-12 23:48:39 -0500

    Added a test for the EventManager.

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/EventManagerTest.java
@@ -0,0 +1,14 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
