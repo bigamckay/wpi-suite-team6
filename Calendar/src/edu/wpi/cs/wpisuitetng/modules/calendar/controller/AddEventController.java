28017b1077706288fe9ff75147061138176bb290
eric <eric@eric-VirtualBox>
2013-11-14 18:13:24 -0500

    Small spelling mistake cleanup, prepare for wipe and repull.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddEventController.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddEventController.java
@@ -6,9 +6,6 @@ import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 public class AddEventController {
 		
 	private AddEventRequestObserver observer;
