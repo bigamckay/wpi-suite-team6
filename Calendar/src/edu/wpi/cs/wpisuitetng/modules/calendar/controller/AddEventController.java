789bfddac0238aad9b4a72d4656b85e396db3527
eric <eric@eric-VirtualBox>
2013-11-14 17:30:02 -0500

    Fixed the build on this branch. This code is written in an effort to see if anything would be sent to the message. A merge with dev will occur before pusing everything in earnest to the server.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddEventController.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddEventController.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.controller;
 
 
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.EventMessage;
 import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
@@ -8,46 +9,26 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class AddEventController implements ActionListener{
+public class AddEventController {
 		
+	private AddEventRequestObserver observer;
+	
 	/**
-	 * Construct an AddEventController for the given model, view pair
-	 * @param model the model containing the messages
-	 * @param view the view where the user enters new messages
+	 * Construct an AddRequirementController for the given model, view pair
 	 */
-	public AddEventController(PostBoardModel model, BoardPanel view) {
-		this.model = model;
-		this.view = view;
-	}
-
-	/* 
-	 * This method is called when the user clicks the Submit button
-	 * 
-	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-	 */
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		// Get the text that was entered
-		String message = view.getTxtNewMessage().getText();
-		
-		// Make sure there is text
-		if (message.length() > 0) {
-			// Clear the text field
-			view.getTxtNewMessage().setText("");
-			
-			// Send a request to the core to save this message
-			final Request request = Network.getInstance().makeRequest("postboard/postboardmessage", HttpMethod.PUT); // PUT == create
-			request.setBody(new PostBoardMessage(message).toJSON()); // put the new message in the body of the request
-			request.addObserver(new AddMessageRequestObserver(this)); // add an observer to process the response
-			request.send(); // send the request
-		}
+	private AddEventController() {
+		observer = new AddEventRequestObserver(this);
 	}
 
 	/**
-	 * When the new message is received back from the server, add it to the local model.
-	 * @param message
+	 * This method adds a requirement to the server.
+	 * @param newRequirement is the requirement to be added to the server.
 	 */
-	public void addMessageToModel(PostBoardMessage message) {
-		model.addMessage(message);
+	public void addEvent(EventMessage newRequirement) 
+	{
+		final Request request = Network.getInstance().makeRequest("calander/eventmessage", HttpMethod.PUT); // PUT == create
+		request.setBody(newRequirement.toJSON()); // put the new requirement in the body of the request
+		request.addObserver(observer); // add an observer to process the response
+		request.send(); 
 	}
 }
--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/EventModel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/EventModel.java
@@ -1,7 +1,96 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.model;
 
-//Make this resemble more of wha tis already in place
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class EventModel {
+import javax.swing.AbstractListModel;
+
+/**
+ * This is a model for the post board. It contains all of the messages
+ * to be displayed on the board. It extends AbstractListModel so that
+ * it can provide the model data to the JList component in the BoardPanel.
+ * 
+ * @author Eric Guleksen
+ *
+ */
+@SuppressWarnings({"serial"})
+public class EventModel extends AbstractListModel {
+	
+	/** The list of all the events on the calendar */
+	private List<EventMessage> events;
+	
+	/**
+	 * Constructs a new board with no messages.
+	 */
+	public EventModel() {
+		events = new ArrayList<EventMessage>();
+	}
+
+	/**
+	 * Adds the given message to the Calendar
+	 * 
+	 * @param newMessage the new message to add
+	 */
+	public void addMessage(EventMessage newMessage) {
+		// Add the message
+		events.add(newMessage);
+		
+		// Notify the model that it has changed so the GUI will be updated
+		this.fireIntervalAdded(this, 0, 0);
+	}
+	
+	/**
+	 * Adds the given array of Events to the Calendar
+	 * 
+	 * @param messages the array of messages to add
+	 */
+	public void addMessages(EventMessage[] messages) {
+		for (int i = 0; i < messages.length; i++) {
+			this.events.add(messages[i]);
+		}
+		this.fireIntervalAdded(this, 0, Math.max(getSize() - 1, 0));
+	}
+	
+	/**
+	 * Removes all Events from this model
+	 * 
+	 * NOTE: One cannot simply construct a new instance of
+	 * the model, because other classes in this module have
+	 * references to it. Hence, we manually remove each message
+	 * from the model.
+	 */
+	public void emptyModel() {
+		int oldSize = getSize();
+		Iterator<EventMessage> iterator = events.iterator();
+		while (iterator.hasNext()) {
+			iterator.next();
+			iterator.remove();
+		}
+		this.fireIntervalRemoved(this, 0, Math.max(oldSize - 1, 0));
+	}
+	
+	/** 
+	 * Returns the message at the given index. This method is called
+	 * internally by the JList in BoardPanel. Note this method returns
+	 * elements in reverse order, so newest messages are returned first.
+	 * 
+	 * @see javax.swing.ListModel#getElementAt(int)
+	 */
+	@Override
+	public Object getElementAt(int index) {
+		return events.get(events.size() - 1 - index).toString();
+	}
+
+	/**
+	 * Returns the number of messages in the model. Also used internally
+	 * by the JList in BoardPanel.
+	 * 
+	 * @see javax.swing.ListModel#getSize()
+	 */
+	@Override
+	public int getSize() {
+		return events.size();
+	}
 
 }
