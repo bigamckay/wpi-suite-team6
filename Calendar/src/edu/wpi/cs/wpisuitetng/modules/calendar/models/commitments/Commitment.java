336b13a642469d16450e909fdf7c33077fd4d230
robynlindsay <robynlindsay@130.215.126.99>
2013-11-18 21:51:28 -0500

    Changed commitment.java to throw exceptions, and changed tests to comply with these changes

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/commitments/Commitment.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/commitments/Commitment.java
@@ -1,16 +1,26 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.models.commitments;
 
 import java.util.Calendar;
+import java.util.UUID;
 
 import com.google.gson.Gson;
 
 //import edu.wpi.cs.wpisuiteng.modules.calendar.commitmentcontroller.UpdateCommitmentController;
 
+
+
+
+import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
 public class Commitment extends AbstractModel{
+	
+	// character limit for name and location field
+	private static final int SHORT_MAX = 40;
+	
 	/** the ID of the Commitment */
-	private int id; // TODO: move ID stuff to server side? (copied from
+	private UUID id; // TODO: move ID stuff to server side? (copied from
 					// requirement impl.)
 
 	/** the name of the commitment */
@@ -19,11 +29,10 @@ public class Commitment extends AbstractModel{
 	/** start and end date associated with the commitment */
 	private Calendar duedate;
 
+	private User creator;
+	
 	public Commitment() {
-		super();
-		id = -1;
-		name = "";
-		duedate = null;
+		this.id = new UUID(0,0);
 	}
 
 	/**
@@ -37,105 +46,133 @@ public class Commitment extends AbstractModel{
 	 * @param due
 	 * 			  The start of the Commitment
 	 */
-	public Commitment(int id, String name, Calendar due) {
-		this.id = id;
+	public Commitment(String name, Calendar due, User creator) throws WPISuiteException{
+		this.id = new UUID(0,0);
+		try{
+			isValidName(name);
+			isValidDate(duedate);
+		}catch(WPISuiteException e){
+			throw e;
+		}
+		
 		this.name = name;
-		if (name.trim().length() == 0)
-			this.name = "Backlog";
 		this.duedate = due;
+		this.creator = creator;
 	}
 	
-	/**
-	 * Constructor for Commitment.
-	 * @param id int
-	 * @param name String
-	 */
-	public Commitment(int id, String name) {
-		this.id = id;
-		this.name = name;
-		if (name.trim().length() == 0)
-			this.name = "Backlog";
-		this.duedate = null;
-	}
-	
-	/**
-	 * Sets the due date for the commitment
-	 * @param start start date
-	 */
-	public void setDueDate(Calendar due) {
-		this.duedate = due;
-	}
 
 	/**
 	 * Getter for the id
 	 * 
 	
 	 * @return the id */
-	public int getId() {
-		return id;
+	public UUID getId() {
+		return this.id;
 	}
 
-	/**
-	 * Setter for the id
-	 * 
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	
 	/**
 	 * getter for the name
 	 * 
 	
 	 * @return the name */
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
-	/**
-	 * setter for the name
-	 * 
-	 * @param name
-	 *            the name to set
-	 */
-//	public void setName(String name) {
-//		this.name = name;
-//		if (name.trim().length() == 0)
-//			this.name = "Backlog";
-//		UpdateCommitmentController.getInstance().updateCommitment(this);
-//	}
-
-	
 
 	/**
 	
 	 * @return the duedate of the commitment */
 	public Calendar getDueDate() {
-		return duedate;
+		return this.duedate;
 	}
 
-	/**
-	 * @param start the start of the commitment
-	 */
-//	public void setDueDate(Calendar due) {
-//		this.duedate = due;
-//		UpdateCommitmentController.getInstance().updateCommitment(this);
+	public User getCreator(){
+		return this.creator;
+	}
+
+//	/**
+//
+//	/**
+//	 * represents commitment as a string which is currently just the name
+//	 * 	
+//	 * @return the name * @see edu.wpi.cs.wpisuitetng.modules.Model#toString() * @see edu.wpi.cs.wpisuitetng.modules.Model#toString() * @see edu.wpi.cs.wpisuitetng.modules.Model#toString()
+//	 */
+//	// should this be extended for the other fields?
+//	public String toString() {
+//		return name;
 //	}
-
-	/**
-
-	/**
-	 * represents commitment as a string which is currently just the name
-	 * 	
-	 * @return the name * @see edu.wpi.cs.wpisuitetng.modules.Model#toString() * @see edu.wpi.cs.wpisuitetng.modules.Model#toString() * @see edu.wpi.cs.wpisuitetng.modules.Model#toString()
-	 */
-	// should this be extended for the other fields?
-	public String toString() {
-		return name;
+	
+	public String setName(String to) throws WPISuiteException{
+		try{
+			isValidName(to);
+		}catch(WPISuiteException e){
+			throw e;
+		}
+		
+		String previous = this.name;
+		this.name = to;
+		return previous;
+	}
+	
+	
+	public Calendar setDueDate(Calendar to) throws WPISuiteException{
+		try{
+			isValidDate(to);
+		}catch (WPISuiteException e){
+			throw e;
+		}
+		
+		Calendar previous = this.duedate;
+		this.duedate = to;
+		return previous;
 	}
 
+	
+	/**
+	 *  Confirms that the name is valid:
+	 *  - cannot exceed SHORT_MAX characters
+	 *  - cannot be NULL
+	 *  - can only contain ascii between 32 and 126 (inclusive)
+	 *  - throws WPI Suite Exception with description if any of these requirements are not fulfilled
+	 *  @param name the name to validate
+	 *  @return 0 on success
+	 */
+	private int isValidName(String name) throws WPISuiteException{
+		//restriction on field length
+		if(name.length() > SHORT_MAX)
+			throw new WPISuiteException("Name too long.");
+		if(name.length() == 0)
+			throw new WPISuiteException("Name cannot be empty."); 
+		// restrictions on characters (ascii 32 - 126 inclusive)
+		for(int i = 1; i<32; i++){
+			String j = "" + (char) i; //cast as char to convert ascii value to character
+			//then append with empty string to convert to string
+			if(name.contains(j))
+				throw new WPISuiteException("Name cannot contain character " + j);
+		}
+		String j = "" + (char) 127;
+		if(name.contains(j))
+			throw new WPISuiteException("Name cannot contain character " + j); //also check for ascii 127
+		//if method hasn't thrown exception by this point, name should be valid. return
+		return 0;
+	}
+	
+	/**
+	 * Confirms that a date is in the future. Events should not be created in the past.
+	 * - throws a WPI Suite Exception with description if invalid
+	 * @param cal the calendar containing the date to be validated
+	 * @return 0 on success
+	 */
+	private int isValidDate(Calendar cal) throws WPISuiteException{
+		// Date has to be in the present or future
+		if(cal.compareTo(Calendar.getInstance()) < 0)
+			throw new WPISuiteException("Events must occur in the future.");
+		return 0;
+	}
+	
+	
 	/**
 	 * compares two commitment for equality based on name
 	 * 
@@ -161,6 +198,7 @@ public class Commitment extends AbstractModel{
 	public void copyFrom(Commitment toCopyFrom) {
 		this.name = toCopyFrom.name;
 		this.duedate = toCopyFrom.duedate;
+		this.creator = toCopyFrom.creator;
 	}
 
 	/**
--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/models/commitments/CommitmentTest.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/models/commitments/CommitmentTest.java
@@ -2,12 +2,14 @@ package edu.wpi.cs.wpisuitetng.modules.calendar.models.commitments;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.UUID;
 
+import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.calendar.models.commitments.*;
 import edu.wpi.cs.wpisuitetng.modules.calendar.MockData;
 import edu.wpi.cs.wpisuitetng.modules.calendar.MockNetwork;
 import edu.wpi.cs.wpisuitetng.modules.calendar.MockRequest;
-
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.configuration.NetworkConfiguration;
 import static org.junit.Assert.*;
@@ -15,23 +17,42 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class CommitmentTest {
-
+	// declare sample commitments
+	Commitment testCommitment;
+	Commitment testCommitment1;
+	Commitment testCommitment2;
+	
 	//set up example calendars
 	Calendar testDueDate = new GregorianCalendar(2014, Calendar.NOVEMBER, 15, 16, 0);
 	Calendar testDueDate1 = new GregorianCalendar(2015, Calendar.NOVEMBER, 15, 17, 0);
+	Calendar testDueDate2 = new GregorianCalendar(2199, Calendar.NOVEMBER, 17, 18, 0);
 	
-	//set up example commitments
-	Commitment testCommitment = new Commitment(2, "thisIsATest", testDueDate);
-	Commitment testCommitment1 = new Commitment(3, "thisIsAlsoATest");
-	Commitment testCommitment2 = new Commitment(4, "thisIsATest", testDueDate1);
-	Commitment blankCommitment = new Commitment(0, "", testDueDate);
+	// set up example user
+	User testUser = new User("Jean Valjean", "jvaljean", "mynameisjeanvaljean", 42601);
 	
+		
 	@Before
 	public void setUp() throws Exception {
 		//make a fake simulated network to test JSON things
 		Network.initNetwork(new MockNetwork());
 		Network.getInstance().setDefaultNetworkConfiguration(
 				new NetworkConfiguration("http://wpisuitetng"));
+		//set up example commitments
+		try{
+			testCommitment = new Commitment("thisIsATest", testDueDate, testUser);
+		}catch(WPISuiteException e){}
+		
+		try{
+			testCommitment1 = new Commitment("thisIsAlsoATest",testDueDate2, testUser);
+		}catch(WPISuiteException e){}
+		
+		try{
+			testCommitment2 = new Commitment("thisIsYetAnotherTest", testDueDate1, testUser);
+		}catch(WPISuiteException e){}
+		
+		
+	
+	
 	}	
 	
 	@Test
@@ -41,49 +62,22 @@ public class CommitmentTest {
 	}
 
 	@Test
-	public void testCommitmentIntStringCalendar() {
-		assertNotNull(testCommitment.getId());
+	public void testCommitmentCompleteConstructor() {
 		assertNotNull(testCommitment.getName());
 		assertNotNull(testCommitment.getDueDate());
-		//NEEDS A WAY TO TEST THAT IT IS AN INT
-		//assertEquals(testCommitment.getId(), Integer.class);  
+		assertNotNull(testCommitment.getCreator());
 		assertTrue(testCommitment.getName() instanceof String); 
 		assertTrue(testCommitment.getDueDate() instanceof Calendar); 
-		fail("NEEDS FIXED!");
 	}
 	
-	@Test
-	public void testCommitmentIntString() {
-		assertNotNull(testCommitment1.getId());
-		assertNotNull(testCommitment1.getName());
-		assertNull(testCommitment1.getDueDate());
-		//NEEDS A WAY TO TEST THAT IT IS AN INT
-		//assertEquals(testCommitment.getId(), Integer.class);  
-		assertTrue(testCommitment.getName() instanceof String); 
-		fail("NEEDS FIXED!");
-	}
 
-	@Test
-	public void testSetDueDate() {
-		assertNotNull(testCommitment.getDueDate());
-		testCommitment.setDueDate(testDueDate1);
-		assertEquals(testCommitment.getDueDate(), testDueDate1);
-	}
 
 	@Test
 	public void testGetId() {
-		assertNotNull(testCommitment.getId());
-		assertTrue(testCommitment.getId()==2);
-		assertFalse(testCommitment.getId() != 2);
+		assertNotNull(testCommitment.getId()); //the ID is not null and...
+		assertNotEquals(testCommitment.getId().compareTo(new UUID(0, 0)), 0); //it's been randomized
 	}
 
-	@Test
-	public void testSetId() {
-		assertNotNull(testCommitment.getId());
-		testCommitment.setId(4);
-		assertEquals(testCommitment.getId(), 4);
-		assertNotEquals(testCommitment.getId(), 2);
-	}
 
 	@Test
 	public void testGetName() {
@@ -96,7 +90,90 @@ public class CommitmentTest {
 		assertNotNull(testCommitment.getDueDate());
 		assertTrue(testCommitment.getDueDate().equals(testDueDate));
 	}
+	
+	@Test
+	public void testGetCreator(){
+		assertNotNull(testCommitment.getCreator());
+		assertEquals(testUser, testCommitment.getCreator());
+	}
 
+	@Test
+	public void testSetName(){
+		assertNotNull(testCommitment.getName());
+		try{
+			testCommitment.setName("This is a new test!");
+		}catch(WPISuiteException e){}
+		
+		assertEquals(testCommitment.getName(), "This is a new test!");
+	}
+	
+	@Test
+	// Test that exception is thrown when entering a null name
+	public void testException_SetName_NULL() {
+		try{
+			testCommitment.setName("");
+			// Should throw exception and not execute the following line
+			assertTrue("Exception not thrown when shouled have.", false);
+		}catch(WPISuiteException e){
+			// confirm that the cause of exception is that the name must exist
+			assertEquals("Exception thrown that event's name can't be empty.", e.getMessage(), "Name cannot be empty.");
+		}
+	}
+	
+	@Test
+	// Test that exception is thrown when entering an invalid character
+	public void testException_SetName_InvalidCharacter() {
+		try{
+			testCommitment.setName("myNameis	");
+			// Should throw exception and not execute the following line
+			assertTrue("Exception not thrown when shouled have.", false);
+		}catch(WPISuiteException e){
+			// confirm that the cause of exception is existence of tab
+			assertEquals("Exception thrown that event's name must have valid character.", e.getMessage(), "Name cannot contain character 	");
+		}
+	}
+	
+	@Test
+	// Test that exception is thrown when entering a really long name
+	public void testException_SetName_NameTooLong() {
+		try{
+			testCommitment.setName("1234567890 1234567890 1234567890 1234567980");
+			// Should throw exception and not execute the following line
+			assertTrue("Exception not thrown when shouled have.", false);
+		}catch(WPISuiteException e){
+			// confirm that the cause of exception is length
+			assertEquals("Exception thrown that event's name has a size limit.", e.getMessage(), "Name too long.");
+		}
+	}
+	
+	
+	
+	@Test
+	public void testSetDueDate() {
+		assertNotNull(testCommitment.getDueDate());
+		try{
+			testCommitment.setDueDate(testDueDate1);
+		}catch(WPISuiteException e){}
+		
+		assertEquals(testCommitment.getDueDate(), testDueDate1);
+	}
+	
+	
+	/* TEST DATE EXCEPTIONS */
+
+	@Test
+	// Test that exception is thrown when trying to set an invalid date
+	public void testException_SetDueDate_DateInPast() {
+		try{
+			testCommitment.setDueDate(new GregorianCalendar(2012, Calendar.JUNE, 10, 18, 0));
+			// Should throw exception and not execute the following line
+			assertTrue("Exception not thrown when shouled have.", false);
+		}catch(WPISuiteException e){
+			// confirm that the cause of exception is a date in the past
+			assertEquals("Exception thrown that events must occur in the future.", e.getMessage(), "Events must occur in the future.");
+		}
+	}
+	
 	@Test
 	public void testToString() {
 		assertNotNull(testCommitment.toString());
@@ -109,23 +186,22 @@ public class CommitmentTest {
 		assertFalse(testCommitment.equalsCommitment(testCommitment1));
 	}
 
-	@Test
-	public void testCopyFrom() {
-		blankCommitment.copyFrom(testCommitment);
-		
-		//assertFalse(testCommitment.equalsCommitment(blankCommitment));
-		assertTrue(testCommitment.getName().equals(blankCommitment.getName()));
-		assertTrue(testCommitment.getDueDate().equals(blankCommitment.getDueDate()));
-		assertNotEquals(testCommitment.getId(), (blankCommitment.getId()));
-	}
+//	@Test
+//	public void testCopyFrom() {
+//		blankCommitment.copyFrom(testCommitment);
+//		
+//		//assertFalse(testCommitment.equalsCommitment(blankCommitment));
+//		assertTrue(testCommitment.getName().equals(blankCommitment.getName()));
+//		assertTrue(testCommitment.getDueDate().equals(blankCommitment.getDueDate()));
+//		assertNotEquals(testCommitment.getId(), (blankCommitment.getId()));
+//	}
 	
 	@Test
 	public void testToFromJson() {
-		Commitment com = new Commitment(12, "jsontest");
-		String jsoncom = com.toJSON();
+		String jsoncom = testCommitment.toJSON();
 		Commitment returned = Commitment.fromJson(jsoncom);
-		assertEquals(returned.getId(), com.getId());
-		assertEquals(returned.getName(), com.getName());
+		assertEquals(returned.getId(), testCommitment.getId());
+		assertEquals(returned.getName(), testCommitment.getName());
 	}
 
 	@Test
