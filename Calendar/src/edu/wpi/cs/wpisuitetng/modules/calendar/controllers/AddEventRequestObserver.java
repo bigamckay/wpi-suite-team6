76936db0517a91d6c701d2cc74f650df832ef171
eric <eric@eric-VirtualBox>
2013-12-09 19:37:31 -0500

    changed requirement to event

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controllers/AddEventRequestObserver.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controllers/AddEventRequestObserver.java
@@ -42,7 +42,7 @@ public class AddEventRequestObserver implements RequestObserver {
 		final ResponseModel response = iReq.getResponse();
 		
 		// Parse the requirement out of the response body
-		final Event requirement = Event.fromJSON(response.getBody(), Event.class);		
+		final Event event = Event.fromJSON(response.getBody(), Event.class);		
 	}
 
 	/**
