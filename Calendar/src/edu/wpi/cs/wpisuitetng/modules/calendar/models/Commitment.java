4c5bae51ee85dee4302b387a6eba40f7191fc4cb
robynlindsay <robynlindsay@130.215.173.140>
2013-12-10 20:54:30 -0500

    Added some javadocs

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/Commitment.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/Commitment.java
@@ -19,6 +19,8 @@ import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.calendar.utils.ValidationUtils;
 import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
+
+
 public class Commitment extends AbstractCalendarModel{
 	
 	private String name; //commitment name
