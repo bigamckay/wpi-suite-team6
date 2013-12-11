a63a512a0c1be5f8eaff0f8a912aaf8f0ddbe782
John French <jdaviesf7@gmail.com>
2013-12-10 22:46:02 -0500

    Fix ID generation so deleting things from the DB doesn't break it

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/entitymanagers/AbstractCalendarEntityManager.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/entitymanagers/AbstractCalendarEntityManager.java
@@ -137,7 +137,11 @@ public abstract class AbstractCalendarEntityManager<T extends AbstractCalendarMo
 
 	@Override
 	public void save(Session s, T model) throws WPISuiteException {
-		model.setId(Count()+1); //set id to next id available
+		int biggestId = 0;
+		for(T t : getAll(s)){
+			biggestId = (t.getId() > biggestId) ? t.getId() : biggestId;
+		}
+		model.setId(biggestId+1); //set id to next id available
 		if(!db.save(model, s.getProject())){
 			throw new WPISuiteException("Could not save to database");
 		} else System.out.println("Saved a model to the database: " + model.toJSON());
