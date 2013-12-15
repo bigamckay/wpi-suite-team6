c13cff2805528e8c8527ec69b7a6ba0e3f65c47d
Craig <canesbitt@wpi.edu>
2013-12-15 14:56:44 -0500

    Copied EventListModel into CommitmentListModel to ensure that all changes made for events also apply to commitments. Also removed two unused imports.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/EventListModel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/models/EventListModel.java
@@ -20,9 +20,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
-import javax.naming.event.EventContext;
 import javax.swing.AbstractListModel;
 
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
